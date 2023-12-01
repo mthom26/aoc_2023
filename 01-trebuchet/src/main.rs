e7bd2f4c653047ca49d24b9c8c1ca270e4759a29
mthom26 <1337nooblet@protonmail.com>
2023-12-01 14:40:05 +0000

    Day 01 part 01

--- 01-trebuchet/src/main.rs
+++ 01-trebuchet/src/main.rs
@@ -1,3 +1,33 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("./input.txt").unwrap();
+
+    let mut sum: usize = 0;
+
+    for line in input.lines() {
+        let filtered: Vec<char> = line.chars().filter(|c| c.is_numeric()).collect();
+        let mut num = String::new();
+
+        if filtered.len() == 0 {
+            panic!("No numeric chars");
+        }
+
+        if filtered.len() == 1 {
+            num.push(filtered[0]);
+            num.push(filtered[0]);
+        } else {
+            num.push(filtered[0]);
+            num.push('0');
+            for c in filtered.iter().skip(1) {
+                num.pop();
+                num.push(*c);
+            }
+        }
+
+        let num = num.parse::<usize>().unwrap();
+        sum += num;
+    }
+
+    println!("Part 01 Sum: {}", sum);
 }
