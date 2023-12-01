bf57edb7f312ddaceff9a5ac41d33be6679df21c
mthom26 <1337nooblet@protonmail.com>
2023-12-01 20:49:20 +0000

    Day 01 finished

--- 01-trebuchet/src/main.rs
+++ 01-trebuchet/src/main.rs
@@ -30,4 +30,64 @@ fn main() {
     }
 
     println!("Part 01 Sum: {}", sum);
+
+    // Part 02
+    let mut sum_part_02 = 0;
+
+    let patterns = [
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six",
+        "seven", "eight", "nine",
+    ];
+
+    for line in input.lines() {
+        let mut pairs_first: Vec<(usize, &str)> = Vec::new();
+        let mut pairs_last: Vec<(usize, &str)> = Vec::new();
+
+        for p in patterns {
+            if let Some(i) = line.find(p) {
+                pairs_first.push((i, p));
+            }
+            if let Some(i) = line.rfind(p) {
+                pairs_last.push((i, p));
+            }
+        }
+
+        pairs_first.sort();
+        pairs_last.sort();
+        pairs_last.reverse();
+
+        let mut num = String::new();
+        // Ugly but works
+        match pairs_first[0].1 {
+            s if s.len() == 1 => num.push_str(s),
+            "one" => num.push('1'),
+            "two" => num.push('2'),
+            "three" => num.push('3'),
+            "four" => num.push('4'),
+            "five" => num.push('5'),
+            "six" => num.push('6'),
+            "seven" => num.push('7'),
+            "eight" => num.push('8'),
+            "nine" => num.push('9'),
+            _ => panic!("This shouldn't happen"),
+        }
+        match pairs_last[0].1 {
+            s if s.len() == 1 => num.push_str(s),
+            "one" => num.push('1'),
+            "two" => num.push('2'),
+            "three" => num.push('3'),
+            "four" => num.push('4'),
+            "five" => num.push('5'),
+            "six" => num.push('6'),
+            "seven" => num.push('7'),
+            "eight" => num.push('8'),
+            "nine" => num.push('9'),
+            _ => panic!("This shouldn't happen"),
+        }
+
+        let num = num.parse::<usize>().unwrap();
+        sum_part_02 += num;
+    }
+
+    println!("Part 01 Sum: {}", sum_part_02);
 }
